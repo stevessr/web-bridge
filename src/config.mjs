824b3fa1175983691fb0a8029d31bfe91600d83c
stevessr <2984716340@qq.com>
2026-09-02 18:41:40 +0800

    feat: add Electron shim patch polling interval

--- src/config.mjs
+++ src/config.mjs
@@ -50,6 +50,7 @@ export function loadConfig() {
     reconnectMinMs: intEnv('WEB_BRIDGE_RECONNECT_MIN_MS', 500, { min: 100, max: 60_000 }),
     reconnectMaxMs: intEnv('WEB_BRIDGE_RECONNECT_MAX_MS', 10_000, { min: 500, max: 120_000 }),
     patchThrottleMs: intEnv('WEB_BRIDGE_PATCH_THROTTLE_MS', 50, { min: 10, max: 5000 }),
+    shimPollMs: intEnv('WEB_BRIDGE_SHIM_POLL_MS', 120, { min: 25, max: 5000 }),
     fullSnapshotIntervalMs: intEnv('WEB_BRIDGE_FULL_SNAPSHOT_INTERVAL_MS', 60_000, { min: 5000, max: 3_600_000 }),
     callTimeoutMs: intEnv('WEB_BRIDGE_CDP_CALL_TIMEOUT_MS', 15_000, { min: 1000, max: 120_000 }),
     maxClients: intEnv('WEB_BRIDGE_MAX_CLIENTS', 4, { min: 1, max: 128 }),
